b7f19babb383c6fd0dc84246f0602ca04e2f7be2
Jakhongir <jakhongir.nishanbaev@gmail.com>
2019-03-31 17:04:39 +0500

    Minor changes

--- Directory1/Directory2/Path.php
+++ Directory1/Directory2/Path.php
@@ -2,10 +2,27 @@
 
 class Path
 {
+    private $path;
+
+    /**
+     * @return mixed
+     */
+    public function getPath()
+    {
+        return $this->path;
+    }
+
+    /**
+     * @param mixed $path
+     */
+    public function setPath($path)
+    {
+        $this->path = $path;
+    }
+
     public function __construct()
     {
         var_dump(__DIR__);
-
-        return __DIR__;
+        $this->setPath(__DIR__);
     }
 }
